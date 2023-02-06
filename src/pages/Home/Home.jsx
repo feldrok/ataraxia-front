d96421db2499f72d5b1021c8b6883e0384c27466
feldrok <matias@moravalero.cl>
2023-02-06 19:32:55 -0300

    carousel animation slide

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Slider from '../../components/Slider'
 
 function Home() {
-    return <div></div>
+    return (
+        <div className="flex justify-center">
+            <Slider />
+        </div>
+    )
 }
 
 export default Home
