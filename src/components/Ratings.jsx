0d42d350d5514fc9d1edcbd68f0723c71394f0f1
Ariel Gonzalez Ayala <119444661+arielgonzalezayala@users.noreply.github.com>
2023-02-17 21:07:19 -0300

    Sistema de valoración listo!

--- src/components/Ratings.jsx
+++ src/components/Ratings.jsx
@@ -1,26 +1,46 @@
 import '@smastrom/react-rating/style.css'
 
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+
 import { Rating } from '@smastrom/react-rating'
-import { useState } from 'react'
+import ratingActions from '../store/ratings/actions'
+import { useParams } from 'react-router'
+
+const { createRating, getProductRating, getUserRating } = ratingActions
 
 export default function Ratings() {
-    const [state, setState] = useState({
-        review: '',
-        rating: 0 // Initial value
-    })
+    const ratingStore = useSelector((store) => store.ratings)
+    const dispatch = useDispatch()
+    const params = useParams()
+    const [rating, setRating] = useState(0)
+    const { id } = params
+    console.log(ratingStore)
 
-    function handleRating(selectedValue) {
-        // 1. Logs the selected rating (1, 2, 3...)
-        console.log(selectedValue)
+useEffect(() => {
+        if(ratingStore.message !== 'Rating encontrado') {
+            dispatch(getProductRating(id))
+        }
+        if (ratingStore.message === 'Rating encontrado') {
+            setRating(ratingStore.productRating?.response)
+        }
+}, [ratingStore])
 
-        // 2. Do something with or without the value...
-
-        // 3. Update Rating UI
-        setState((prevState) => ({
-            ...prevState,
-            rating: selectedValue
-        }))
+const handleChange = (selectedValue) => {
+    try {
+        dispatch(createRating({product_id: id, rating: selectedValue}))
+    } catch (error) {
+        console.log(error)
+    } finally {
+        dispatch(getProductRating(id))
     }
+}
 
-    return <Rating style={{ maxWidth: 150, padding: 0 }} onChange={handleRating} value={state.rating} />
+console.log(rating)
+    return (
+    <div className='flex gap-4 items-center'>
+        <Rating style={{ maxWidth: 150, padding: 0 }} onChange={handleChange} value={Math.floor(rating)}  />
+        <p>{`(${rating})`}</p>
+    </div>
+    )
 }
